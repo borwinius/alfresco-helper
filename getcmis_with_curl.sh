947d78ea72d5e85264f3a6af3e6fb30dd09d5611
borwinius <richard@borwinius.de>
2025-05-26 09:58:15 +0200

    get a cmisquery with curl
    change your cmisquery in line 6
    change your user:password and servername in line 9 before

---
+++ getcmis_with_curl.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+# getcmis_with_curl per xml / js
+
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'>query.xml
+echo '<cmis:query xmlns:cmis="http://docs.oasis-open.org/ns/cmis/core/200908/">'>>query.xml
+echo "<cmis:statement>SELECT cmis:name from st:site WHERE cmis:name ='software*'</cmis:statement>">>query.xml
+echo '</cmis:query>' >>query.xml
+
+curl -u admin:admin  -v -X POST http://myalfsrv.my.dom.ain:8080/alfresco/s/cmis/queries -H "Content-Type: application/cmisquery+xml" -d @query.xml
+rm query.xml
